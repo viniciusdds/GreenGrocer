9b66adb58af66fbcf6a2c8598e367d7a96150eef
viniciusdds <viniciusdamasceno180@hotmail.com>
2022-08-14 17:49:35 -0300

    Aula 15

---
+++ android/app/src/main/java/br/com/damasceno/greengrocer/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.damasceno.greengrocer;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,204 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/auth/components/custom_text_field.dart';
+import 'package:greengrocer/src/auth/sign_up_screen.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+
+class SignInScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: CustomColors.customSwatchColor,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Column(
+            children: [
+              Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+
+                       // Nome do App
+                       Text.rich(
+                           TextSpan(
+                             style: const TextStyle(
+                               fontSize: 40
+                             ),
+                             children: [
+                               const TextSpan(
+                                 text: 'Green',
+                                 style: TextStyle(
+                                   color: Colors.white,
+                                   fontWeight: FontWeight.bold
+                                 )
+                               ),
+                               TextSpan(
+                                   text: 'grocer',
+                                   style: TextStyle(
+                                       color: CustomColors.customContrastColor,
+                                       fontWeight: FontWeight.bold
+                                   )
+                               )
+                             ]
+                           )
+                       ),
+
+                       // Categorias
+                       SizedBox(
+                         height: 30,
+                         child: DefaultTextStyle(
+                           style: const TextStyle(
+                             fontSize: 25
+                           ),
+                           child: AnimatedTextKit(
+                               repeatForever: true,
+                               pause: Duration.zero,
+                               animatedTexts: [
+                                 FadeAnimatedText('Frutas'),
+                                 FadeAnimatedText('Verduras'),
+                                 FadeAnimatedText('Legumes'),
+                                 FadeAnimatedText('Carnes'),
+                                 FadeAnimatedText('Cereais'),
+                                 FadeAnimatedText('Laticíneos'),
+                               ]
+                           ),
+                         ),
+                       )
+                    ],
+                  )
+              ),
+              // Formulário
+              Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 40
+                  ),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(45)
+                    )
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+
+                      // Email
+                      CustomTextField(
+                        icon: Icons.email,
+                        label: 'Email',
+                      ),
+
+                      // Senha
+                      CustomTextField(
+                        icon: Icons.lock,
+                        label: 'Senha',
+                        isSecret: true,
+                      ),
+
+                      // Botão de entrar
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18)
+                              )
+                            ),
+                            onPressed: (){},
+                            child: const Text(
+                                "Entrar",
+                                style: TextStyle(
+                                  fontSize: 18
+                                ),
+                            )
+                        ),
+                      ),
+
+                      // Esqueceu a senha
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                            onPressed: (){},
+                            child:  Text(
+                                "Esqueceu a senha?",
+                                style: TextStyle(
+                                  color: CustomColors.customContrastColor
+                                ),
+                            )
+                        ),
+                      ),
+
+                      // Divisor
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 15),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Divider(
+                                color: Colors.grey.withAlpha(90),
+                                thickness: 2,
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 15),
+                              child: Text("OU"),
+                            ),
+                            Expanded(
+                              child: Divider(
+                                color: Colors.grey.withAlpha(90),
+                                thickness: 2,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      // Botão de novo usuário
+                      SizedBox(
+                        height: 50,
+                        child: OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18)
+                              ),
+                              side: const BorderSide(
+                                width: 2,
+                                color: Colors.green
+                              )
+                            ),
+                            onPressed: (){
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                    builder: (c){
+                                      return SignUpScreen();
+                                    }
+                                )
+                              );
+                            },
+                            child: const Text(
+                                "Criar conta",
+                                style: TextStyle(
+                                  fontSize: 18
+                                ),
+                            )
+                        ),
+                      )
+
+
+                    ],
+                  ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
