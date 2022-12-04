9d3c5f50887542431f649f7d274bb3ae0a3c7e18
viniciusdds <viniciusdamasceno180@hotmail.com>
2022-12-04 18:43:52 -0300

    aula 78

---
+++ lib/src/constants/endpoints.dart
@@ -0,0 +1,9 @@
+
+const String baseUrl = 'https://parseapi.back4app.com/functions/';
+
+abstract class Endpoints {
+
+  static const String sigin = '$baseUrl/login';
+  static const String signup = '$baseUrl/signup';
+
+}
