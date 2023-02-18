38211b07adcecf3bb39c34f6dd536e30e01998f2
viniciusdds <viniciusdamasceno180@hotmail.com>
2023-02-17 22:25:36 -0300

    aula 95

--- lib/src/pages/home/home_tab.dart
+++ lib/src/pages/home/home_tab.dart
@@ -2,12 +2,14 @@ import 'package:add_to_cart_animation/add_to_cart_animation.dart';
 import 'package:add_to_cart_animation/add_to_cart_icon.dart';
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/pages/common_widgets/app_name_widget.dart';
 import 'package:greengrocer/src/pages/common_widgets/custom_shimmer.dart';
 import 'package:greengrocer/src/pages/home/components/category_tile.dart';
 import 'package:greengrocer/src/config/app_data.dart' as appData;
 import 'package:greengrocer/src/pages/home/components/item_tile.dart';
+import 'package:greengrocer/src/pages/home/controller/home_controller.dart';
 import 'package:greengrocer/src/services/utils_services.dart';
 
 class HomeTab extends StatefulWidget {
@@ -35,6 +37,8 @@ class _HomeTabState extends State<HomeTab> {
   @override
   void initState() {
     super.initState();
+
+    Get.find<HomeController>().printExample();
 
     Future.delayed(const Duration(
       seconds: 3
--- lib/src/pages_routes/app_pages.dart
+++ lib/src/pages_routes/app_pages.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:greengrocer/src/pages/home/binding/home_binding.dart';
 import '../pages/auth/view/sign_in_screen.dart';
 import '../pages/auth/view/sign_up_screen.dart';
 import 'package:greengrocer/src/pages/base/base_screen.dart';
@@ -6,10 +7,25 @@ import 'package:greengrocer/src/pages/splash/splash_screen.dart';
 
 abstract class AppPages {
   static final pages = <GetPage>[
-    GetPage(page: () => SplashScreen(), name: PagesRoutes.splashRoute),
-    GetPage(page: () => SignInScreen(), name: PagesRoutes.signInRoute),
-    GetPage(page: () => SignUpScreen(), name: PagesRoutes.signUpRoute),
-    GetPage(page: () => BaseScreen(), name: PagesRoutes.baseRoute),
+    GetPage(
+        page: () => SplashScreen(),
+        name: PagesRoutes.splashRoute
+    ),
+    GetPage(
+        page: () => SignInScreen(),
+        name: PagesRoutes.signInRoute
+    ),
+    GetPage(
+        page: () => SignUpScreen(),
+        name: PagesRoutes.signUpRoute
+    ),
+    GetPage(
+        page: () => BaseScreen(),
+        name: PagesRoutes.baseRoute,
+        bindings: [
+          HomeBinding(),
+        ]
+    ),
   ];
 }
 
